7c9735e5da14567ee917fbc06fd107dfc0fa318e
Tito Chan <arlentitotnpm@gmail.com>
2026-03-19 10:13:45 -0500

    Formatter and clear

--- js/app.js
+++ js/app.js
@@ -1,94 +1,87 @@
-var title = document.getElementById("title");
-var layoutControls = document.getElementById("layoutControls");
+var title = document.getElementById("title")
+var layoutControls = document.getElementById("layoutControls")
 
-var configurationBtn = document.getElementById("configurationBtn");
-var controlCompared = document.getElementById("controlCompared");
-var viewsContainer = document.getElementById("viewsContainer");
+var configurationBtn = document.getElementById("configurationBtn")
+var controlCompared = document.getElementById("controlCompared")
+var viewsContainer = document.getElementById("viewsContainer")
 
-var configurationPanel = document.getElementById("configurationPanel");
-var rangeColorOptions = document.querySelectorAll("#rangeColorOption");
-var canvas = document.getElementById("gpuCanvas");
+var configurationPanel = document.getElementById("configurationPanel")
+var rangeColorOptions = document.querySelectorAll("#rangeColorOption")
+var canvas = document.getElementById("gpuCanvas")
 var shaderCode;
 
 // & Modal Config Elements
 
-var selectDialog = document.getElementById("selectDialog");
+var selectDialog = document.getElementById("selectDialog")
 
-var promCheck = document.getElementById("promCheck");
-var rangeRed = document.getElementById("rangeRed");
-var rangeGreen = document.getElementById("rangeGreen");
-var rangeBlue = document.getElementById("rangeBlue");
-var resetBtn = document.getElementById("resetBtn");
+var promCheck = document.getElementById("promCheck")
+var rangeRed = document.getElementById("rangeRed")
+var rangeGreen = document.getElementById("rangeGreen")
+var rangeBlue = document.getElementById("rangeBlue")
+var resetBtn = document.getElementById("resetBtn")
+
 
 var configs = [
-  {
+   {
     name: "Natural",
-    config: {
+    config:{
       xProm: true,
-      red: 2.0,
-      green: 2.0,
-      blue: 2.0,
-    },
+      red:2.00,
+      green:2.00,
+      blue:2.00
+    }
     // red:2.00,
     // green:2.00,
     // blue:2.00
   },
   {
     name: "Cyberpunk",
-    config: {
+    config:{
       xProm: false,
-      red: 3.0,
-      green: 3.0,
-      blue: 3.0,
-    },
+      red:3.00,
+      green:3.00,
+      blue:3.00
+    }
   },
   {
     name: "Brillo",
-    config: {
+    config:{
       xProm: false,
-      red: 2.0,
-      green: 2.0,
-      blue: 2.0,
-    },
-  },
-];
+      red:2.00,
+      green:2.00,
+      blue:2.00
+    }
+  }
+]
 
-initConfig();
+initConfig()
 
 // & variables
 
 var checkProm = true;
-var valueColorRed = 2.0;
-var valueColorGreen = 2.0;
-var valueColorBlue = 2.0;
+var valueColorRed = 2.00;
+var valueColorGreen = 2.00;
+var valueColorBlue = 2.00;
 // var valueColorExposure = 0;
 
-// async function loadShader(path) {
-//   const response = await fetch(path);
-//   return await response.text();
-// }
-loadShader("shader/shader.wgsl");
+loadShader("shader/shader.wgsl")
 
 async function loadShader(path) {
   const response = await fetch(path);
   shaderCode = await response.text();
-  // shaderCode = shaderCode.replace("/valueRedRGB",valueColorRed)
-  // shaderCode = shaderCode.replace("/valueGreenRGB",valueColorGreen)
-  // shaderCode = shaderCode.replace("/valueBlueRGB",valueColorBlue)
-  // console.log(shaderCode);
 }
 
-var adapter;
-var device;
-var paramsBuffer;
-var boolsBuffer;
-const paramsValues = new Float32Array(4);
+var adapter
+var device
+var paramsBuffer
+var boolsBuffer
+const paramsValues = new Float32Array(4); 
 const boolValues = new Uint32Array(4);
-initGpu();
+initGpu()
 async function initGpu() {
   if (!navigator.gpu) {
     alert("Sin soporte para WebGPU");
-    setTitle("Error: Sin soporte para WebGPU");
+    setTitle("Error: Sin soporte para WebGPU")
     return;
   }
   adapter = await navigator.gpu.requestAdapter();
@@ -99,7 +92,7 @@ async function initGpu() {
   });
 
   boolsBuffer = device.createBuffer({
-    size: 16,
+    size: 16, 
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
   });
 
@@ -111,44 +104,45 @@ async function initGpu() {
   device.queue.writeBuffer(boolsBuffer, 0, boolValues);
 }
 
-function initConfig() {
+function initConfig(){
   let i = 0;
-  selectDialog.innerHTML = "";
+  selectDialog.innerHTML = ""
   for (const config of configs) {
-    selectDialog.innerHTML += `<option value="${i}" >${config.name}</option>`;
-    i++;
+    selectDialog.innerHTML += `<option value="${i}" >${config.name}</option>`
+    i++
   }
 }
 
 selectDialog.addEventListener("change", (e) => {
-  loadConfig(e.target.value);
-});
+  loadConfig(e.target.value)
+})
 promCheck.addEventListener("change", (e) => {
-  checkProm = e.target.checked;
-  updateShaderBuffer();
-});
+  checkProm = e.target.checked
+  updateShaderBuffer()
+})
 
-function loadConfig(index) {
+
+function loadConfig(index){
   // ^ Valores
-  checkProm = configs[index].config.xProm;
-  valueColorRed = configs[index].config.red;
-  valueColorGreen = configs[index].config.green;
-  valueColorBlue = configs[index].config.blue;
-
+  checkProm = configs[index].config.xProm
+  valueColorRed = configs[index].config.red
+  valueColorGreen = configs[index].config.green
+  valueColorBlue = configs[index].config.blue
+  
   // ^ Actualizar interfaz
-  promCheck.checked = checkProm;
-  rangeRed.updateValue(valueColorRed);
-  rangeGreen.updateValue(valueColorGreen);
-  rangeBlue.updateValue(valueColorBlue);
+  promCheck.checked = checkProm
+  rangeRed.updateValue(valueColorRed)
+  rangeGreen.updateValue(valueColorGreen)
+  rangeBlue.updateValue(valueColorBlue)
 
-  updateShaderBuffer();
+  updateShaderBuffer()
 }
 
-function updateShaderBuffer() {
-  sendShaderBuffer(valueColorRed, valueColorGreen, valueColorBlue, checkProm);
+function updateShaderBuffer(){
+  sendShaderBuffer(valueColorRed,valueColorGreen,valueColorBlue,checkProm)
 }
 
-function sendShaderBuffer(r, g, b, xProm) {
+function sendShaderBuffer(r, g, b,xProm) {
   paramsValues[0] = r;
   paramsValues[1] = g;
   paramsValues[2] = b;
@@ -165,60 +159,58 @@ function sendShaderBuffer(r, g, b, xProm) {
 
 function keyboardEvent(value, code) {
   if (value == 67) {
-    configurationPanel.showModal();
+    configurationPanel.showModal()
   }
 }
 
 configurationBtn.addEventListener("click", () => {
   configurationPanel.showModal();
-});
+})
 
 resetBtn.addEventListener("click", () => {
-  loadConfig(selectDialog.value);
-  console.log("Reset");
-});
+  loadConfig(selectDialog.value)
+  console.log("Reset")
+})
 // configurationPanel.showModal();
 // console.log("90".parseFloat.toFixed(2))
 for (const rangeColorOption of rangeColorOptions) {
-  let input = rangeColorOption.querySelector("input");
-  let value = rangeColorOption.querySelector("span");
-  input.labelValue = value;
+  let input = rangeColorOption.querySelector("input")
+  let value = rangeColorOption.querySelector("span")
+  input.labelValue = value
 
-  input.updateValue = function (newValue) {
+  input.updateValue = function(newValue) {
     this.value = newValue; // Se actualiza a sí mismo
     // let formattedValue = parseFloat(this.value).toFixed(2);
     this.labelValue.innerHTML = parseFloat(this.value).toFixed(2); // Actualiza su span vinculado
-  };
+  }
 
   input.addEventListener("input", (e) => {
     let inputValue = parseFloat(input.value).toFixed(2);
-    value.innerHTML = inputValue;
+    value.innerHTML = inputValue
     if (e.target.id == "rangeRed") {
-      valueColorRed = inputValue;
+      valueColorRed = inputValue
     } else if (e.target.id == "rangeGreen") {
-      valueColorGreen = inputValue;
+      valueColorGreen = inputValue
     } else if (e.target.id == "rangeBlue") {
-      valueColorBlue = inputValue;
+      valueColorBlue = inputValue
     }
-    updateShaderBuffer();
-  });
+    updateShaderBuffer()
+  })
 }
 
 layoutControls.addEventListener("click", (e) => {
   if (e.target.nodeName == "BUTTON") {
     // for(layoutControls.children)
     // console.log(layoutControls.children);
-
     // console.log(e.target);
     let buttonId = e.target.id;
-    if (buttonId == "configurationBtn") return;
-    changeLayout(buttonId);
+    if(buttonId == "configurationBtn") return;
+    changeLayout(buttonId)
     // e.target.classList.toggle("activeBtn")
-
     // console.log(buttonId)
   }
   // console.log(e);
-});
+})
 
 function setTitle(text) {
   title.innerHTML = text;
@@ -231,11 +223,11 @@ function changeLayout(layout) {
 
   for (const element of elements) {
     if (element.id == layout) {
-      element.classList.add("activeBtn");
-      viewsContainer.classList.add(layout + "Status");
+      element.classList.add("activeBtn")
+      viewsContainer.classList.add(layout + "Status")
     } else {
-      element.classList.remove("activeBtn");
-      viewsContainer.classList.remove(element.id + "Status");
+      element.classList.remove("activeBtn")
+      viewsContainer.classList.remove(element.id + "Status")
     }
   }
 }
@@ -243,42 +235,43 @@ function changeLayout(layout) {
 var mousedown = false;
 controlCompared.addEventListener("mousedown", (e) => {
   mousedown = true;
-});
+})
 
 controlCompared.addEventListener("touchstart", (e) => {
   mousedown = true;
-});
+})
 
 // Mostrar todos los evento del comparador
 viewsContainer.addEventListener("mousemove", (e) => {
   if (mousedown) {
     var widthView = e.view.innerWidth;
     var positionX = e.clientX;
-    var porcentaje = (positionX / widthView) * 100;
+    var porcentaje = (positionX / widthView) * 100
     // console.log(`WidthView: ${widthView} | PositionX: ${positionX} | % : ${(positionX/widthView)*100}%`);
     canvas.style.width = porcentaje + "%";
-    controlCompared.style.left = porcentaje + "%";
+    controlCompared.style.left = porcentaje + "%"
     // console.log(e)
   }
-});
+})
 
 viewsContainer.addEventListener("touchmove", (e) => {
   if (mousedown) {
     var widthView = e.view.innerWidth;
     var positionX = e.touches[0].clientX;
-    var porcentaje = (positionX / widthView) * 100;
-    canvas.style.width = porcentaje + "%";
-    controlCompared.style.left = porcentaje + "%";
+    var porcentaje = (positionX / widthView) * 100
+    canvas.style.width = porcentaje + "%"
+    controlCompared.style.left = porcentaje + "%"
   }
-});
+})
 
 viewsContainer.addEventListener("mouseup", () => {
-  mousedown = false;
-});
+  mousedown = false
+})
 
 controlCompared.addEventListener("touchend", (e) => {
-  mousedown = false;
-});
+  mousedown = false
+})
+
 controlCompared.addEventListener("touchcancel", (e) => {
-  mousedown = false;
-});
+  mousedown = false
+})
